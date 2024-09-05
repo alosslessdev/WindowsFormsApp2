1c01060b621ee3c8c0bdac96a737ddee40987434
alosslessdev <ppaanncchhoo507@gmail.com>
2024-09-05 18:57:57 -0500

    unir

---
+++ WindowsFormsApp3/Problema2Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Problema2Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources shoo be disposed; osdlsdtherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(692, 427);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 72);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+<<<<<<< HEAD:WindowsFormsApp3/Problema2Form1.Designer.cs
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(175, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+=======
+>>>>>>> c9d28b296167b1a6bd1660776324dc59d9d858a1:WindowsFormsApp3/Form1.Designer.cs
+            // 
+            // Problema2Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 541);
+            this.Controls.Add(this.button1);
+<<<<<<< HEAD:WindowsFormsApp3/Problema2Form1.Designer.cs
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Problema2Form1";
+            this.Text = "Problema2Form1";
+=======
+            this.Name = "Form1";
+            this.Text = "Form1";
+>>>>>>> c9d28b296167b1a6bd1660776324dc59d9d858a1:WindowsFormsApp3/Form1.Designer.cs
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp3/Problema2Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Problema2Form1 : Form
+    {
+        public Problema2Form1()
+        {
+            InitializeComponent();
+        }
+
+<<<<<<< HEAD:WindowsFormsApp3/Problema2Form1.cs
+        private int i, j, y, x, numeroAlRevesInt, numeroAlDerechoInt, resultado1, resultado2;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (i = 0; i <= Convert.ToInt32(textBox1.Text); i++)
+            {
+                //para cada numero natural hasta el numero del usuario
+
+                for (j = 0; j <= textBox1.TextLength-1; j++)
+                {   //revisar numero desde indice 0/izquierda
+
+                    numeroAlDerechoString = String.Join("", textBox1.Text[j]);
+                    //unir todos los caracteres del textbox de izquierda a derecha
+
+                    resultado1 = Convert.ToInt32(numeroAlDerechoString);
+
+                }
+
+                for (x = textBox1.TextLength-1; x >= 0; x--)
+                {   //revisar numero desde el ultimo indice/derecha
+
+                    numeroAlRevesString = String.Join("", textBox1.Text[x]);
+                    //unir todos los caracteres del textbox de derecha a izquierda
+
+                    resultado2 = Convert.ToInt32(numeroAlRevesString);
+
+
+                }
+
+                if (resultado1 == resultado2)
+                {
+                    //si se lee igual desde derecha y desde izquierda
+
+                    listBox1.Items.Add(resultado1);
+                    //no deberia importar si el numero sale de numeroAlDerechoString o de
+                    //numeroAlRevesString
+
+                }
+
+            }
+        }
+
+        private int[] arregloOriginal;
+        private int[] arregloCopia;
+
+        private string entrada, numeroAlRevesString, numeroAlDerechoString;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+=======
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 nuevoForm = new Form2();
+            nuevoForm.Show();
+            this.Hide();
+>>>>>>> c9d28b296167b1a6bd1660776324dc59d9d858a1:WindowsFormsApp3/Form1.cs
+        }
+    }
+}
