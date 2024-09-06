9bc8a7737d78ad6ab15cb3da89be5afcc4f7e4e2
alosslessdev <ppaanncchhoo507@gmail.com>
2024-09-05 20:33:53 -0500

    empezar a unir

--- WindowsFormsApp3/Problema2Form1.Designer.cs
+++ WindowsFormsApp3/Problema2Form1.Designer.cs
@@ -29,51 +29,70 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(692, 427);
+            this.button1.Location = new System.Drawing.Point(56, 161);
+            this.button1.Margin = new System.Windows.Forms.Padding(1);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(199, 72);
+            this.button1.Size = new System.Drawing.Size(84, 31);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-<<<<<<< HEAD:WindowsFormsApp3/Problema2Form1.Designer.cs
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(175, 59);
+            this.textBox1.Location = new System.Drawing.Point(144, 61);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 3;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-=======
->>>>>>> c9d28b296167b1a6bd1660776324dc59d9d858a1:WindowsFormsApp3/Form1.Designer.cs
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(341, 76);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 84);
+            this.listBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Numeros del 0 al:";
             // 
             // Problema2Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(943, 541);
+            this.ClientSize = new System.Drawing.Size(599, 429);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-<<<<<<< HEAD:WindowsFormsApp3/Problema2Form1.Designer.cs
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(1);
             this.Name = "Problema2Form1";
             this.Text = "Problema2Form1";
-=======
-            this.Name = "Form1";
-            this.Text = "Form1";
->>>>>>> c9d28b296167b1a6bd1660776324dc59d9d858a1:WindowsFormsApp3/Form1.Designer.cs
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
